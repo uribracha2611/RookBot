b1e894903e480d1c8049f32feec76d6d27079df0
uribracha2611 <uribracha2611@gmail.com>
2025-08-14 16:16:35 +0300

    Revert "added staged movegen(buggy)"
    This reverts commit 80ded8b576fb3c3b506a27ae1ef4a0464ecba83f.

--- src/engine/movegen/mod.rs
+++ src/engine/movegen/mod.rs
@@ -4,4 +4,3 @@ pub mod magic;
 pub mod movedata;
 pub mod movelist;
 pub mod precomputed;
-pub mod movepicker;
--- src/engine/movegen/movelist.rs
+++ src/engine/movegen/movelist.rs
@@ -1,51 +1,9 @@
-use crate::engine::board::board::Board;
-use crate::engine::board::see::static_exchange_evaluation;
 use crate::engine::movegen::constants::MAX_MOVES;
 use crate::engine::movegen::movedata::MoveData;
-use crate::engine::search::constants::INFINITY;
 
-#[derive(Copy, Clone)]
-pub struct MoveEntry {
-    mv: MoveData,
-    see_score: i32,
-    score: i32,
-}
-impl Default for MoveEntry {
-    fn default() -> Self {
-        MoveEntry { see_score: 0, mv: MoveData::default(), score: 0 }
-    }
-}
-impl MoveEntry {
-    pub fn get_mv(&self) -> MoveData
-    {
-        self.mv
-    }
-    pub fn get_score(&self) -> i32 {
-        self.score
-    }
-
-    pub fn entry_from_mv(mv: MoveData) -> MoveEntry {
-        MoveEntry { mv, see_score: 0, score: 0 }
-    }
-    pub fn calc_see_score(&mut self, board: &Board) {
-        self.see_score = static_exchange_evaluation(board, &self.mv);
-    }
-    pub fn set_see_score(&mut self, see_score: i32) {
-        self.see_score = see_score;
-    }
-    pub fn set_score(&mut self, score: i32) {
-        self.score = score;
-    }
-    pub fn get_see_score(&self) -> i32 {
-        self.see_score
-    }
-    pub fn default() -> MoveEntry {
-        MoveEntry { mv: MoveData::default(), see_score: -INFINITY, score: -INFINITY }
-    }
-}
 #[derive(Copy, Clone)]
 pub struct MoveList {
-    moves: [MoveEntry; MAX_MOVES],
+    moves: [MoveData; MAX_MOVES],
     count: usize,
 }
 
@@ -56,10 +14,6 @@ impl Default for MoveList {
 }
 
 impl MoveList {
-    pub fn is_empty(&self) -> bool {
-        self.count == 0
-    }
-
     pub fn iter(&self) -> MoveListIterator {
         MoveListIterator {
             movelist: self,
@@ -71,7 +25,7 @@ impl MoveList {
     }
     pub fn new() -> Self {
         MoveList {
-            moves: [MoveEntry::default(); MAX_MOVES],
+            moves: [MoveData::default(); MAX_MOVES],
             count: 0,
         }
     }
@@ -83,24 +37,19 @@ impl MoveList {
         }
     }
 
-    pub fn add_move(&mut self, mv: MoveData, index: &mut usize) {
+    pub fn add_move(&mut self, mv: MoveData) {
         if self.count < MAX_MOVES {
-            self.moves[*index] = MoveEntry::entry_from_mv(mv);
-            *index += 1;
+            self.moves[self.count] = mv;
             self.count += 1;
         }
     }
 
-
-    pub fn get_move(&self, index: usize) -> &MoveEntry {
-        &self.moves[index]
-    }
-
-    pub fn pop_move(&mut self, index: usize) -> MoveEntry {
-        let ret_move = self.moves[index];
-        self.count -= 1;
-        self.moves[index] = self.moves[self.count];
-        ret_move
+    pub fn get_move(&self, index: usize) -> &MoveData {
+        if index < self.count {
+            &self.moves[index]
+        } else {
+            panic!("Index out of bounds");
+        }
     }
 
     pub fn move_count(&self) -> usize {
@@ -108,13 +57,13 @@ impl MoveList {
     }
 
     pub fn is_move_in_list(&self, mv: &MoveData) -> bool {
-        self.moves.iter().take(self.count).any(|mv_entry| mv_entry.mv == *mv)
+        self.moves.iter().take(self.count).any(|m| *m == *mv)
     }
     pub fn find_move_by_start_end_square(self, from: u8, to: u8) -> Option<MoveData> {
         for i in 0..MAX_MOVES {
-            if let mv_entry = self.moves[i] {
-                if mv_entry.mv.from == from && mv_entry.mv.to == to {
-                    return Some(mv_entry.mv);
+            if let mv = self.moves[i] {
+                if mv.from == from && mv.to == to {
+                    return Some(mv);
                 }
             }
         }
@@ -123,7 +72,7 @@ impl MoveList {
 }
 
 impl std::ops::Index<usize> for MoveList {
-    type Output = MoveEntry;
+    type Output = MoveData;
 
     fn index(&self, index: usize) -> &Self::Output {
         &self.moves[index]
@@ -135,8 +84,9 @@ impl std::ops::IndexMut<usize> for MoveList {
         &mut self.moves[index]
     }
 }
+
 impl<'a> IntoIterator for &'a MoveList {
-    type Item = &'a MoveEntry;
+    type Item = &'a MoveData;
     type IntoIter = MoveListIterator<'a>;
 
     fn into_iter(self) -> Self::IntoIter {
@@ -153,10 +103,10 @@ pub struct MoveListIterator<'a> {
 }
 
 impl<'a> Iterator for MoveListIterator<'a> {
-    type Item = &'a MoveEntry;
+    type Item = &'a MoveData;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index < MAX_MOVES {
+        if self.index < self.movelist.count {
             let result = self.movelist.get_move(self.index);
             self.index += 1;
             Option::from(result)
