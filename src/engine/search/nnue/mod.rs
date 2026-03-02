26e300582d3fe83bbfe147ee5abc5b0c94da0282
uribracha <uribracha2611@gmail.com>
2026-03-02 17:38:31 +0200

    fixed my network

--- src/engine/search/nnue/mod.rs
+++ src/engine/search/nnue/mod.rs
@@ -3,4 +3,4 @@ use crate::engine::search::nnue::types::Network;
 pub mod simd;
 pub mod types;
 pub static NNUE_NETWORK: Network =
-    unsafe { std::mem::transmute(*include_bytes!("../../../beans.bin")) };
+    unsafe { std::mem::transmute(*include_bytes!("../../../nnue.bin")) };
