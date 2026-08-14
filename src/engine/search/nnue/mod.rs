19b3d6ba9ed08d3adf93a83120e10e8dffe755c6
uribracha <uribracha2611@gmail.com>
2026-08-14 10:52:45 +0300

    new net (256HL trained on 5M positions
    107.26 +/- 27.77 elo

--- src/engine/search/nnue/mod.rs
+++ src/engine/search/nnue/mod.rs
@@ -2,4 +2,4 @@ use crate::engine::search::nnue::types::Network;
 pub mod simd;
 pub mod types;
 pub static NNUE_NETWORK: Network =
-    unsafe { std::mem::transmute(*include_bytes!("./nets/nnue-256.bin")) };
+    unsafe { std::mem::transmute(*include_bytes!("./nets/nnue_256_v2.bin")) };
